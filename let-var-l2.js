671aa83eb6384604cae7fa2a2e6d5b3fabfcfa8c
liubovsergheeva <84126939+liubovsergheeva@users.noreply.github.com>
2023-03-10 21:59:08 +0300

    Add files via upload

---
+++ let-var-l2.js
@@ -0,0 +1,19 @@
+var count = 0
+// Ctrl +c остановить бесконечный цикл
+// while (true) {
+//     console.log (count)
+//     count++
+// }
+
+// выведем в цикле while таблицу сравнения переменных v2 и v1, при условии что v1++ инкрементируется на каждой итерации на 1
+
+var v1 = 10
+var v2=20
+var v3=100
+var r1=30
+while (v2>v1){
+    console.log(count, v1, v2)
+    count++
+    v1++
+
+}
